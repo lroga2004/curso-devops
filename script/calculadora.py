e08f0d6b9301d9e35ff07cf7364366d3868ec194
lroga2004 <lroga2004@gmail.com>
2026-08-26 12:07:06 +0200

    fix calculadora.py

--- script/calculadora.py
+++ script/calculadora.py
@@ -14,23 +14,3 @@ def dividir(a, b):
     if b == 0:
         return "No se puede dividir entre 0"
     return a / b
-
-
-print("=== Mi Calculadora DevOps ===")
-
-a = float(input("Introduce el primer número: "))
-operacion = input("Operación (+, -, *, /): ")
-b = float(input("Introduce el segundo número: "))
-
-if operacion == "+":
-    resultado = sumar(a, b)
-elif operacion == "-":
-    resultado = restar(a, b)
-elif operacion == "*":
-    resultado = multiplicar(a, b)
-elif operacion == "/":
-    resultado = dividir(a, b)
-else:
-    resultado = "Operación no válida"
-
-print(f"Resultado: {resultado}")
